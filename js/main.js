562930848be791a6b5c4c5496f57d1e0f6f4a116
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-03-07 15:30:21 -0300

    agrega estrcutura y estilos iniciales para simon-dice

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const sequenciaMaquina = [];
+const sequenciaUsuario = [];
+const turno = 0;
+
+function iniciar(){
+  document.querySelector('button') = 12;
+}
+
+document.querySelector('button').onclick = function(e) {
+  alert('hi');
+
+  e.preventDefault()
+}
