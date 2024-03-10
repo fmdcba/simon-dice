f9487ad0730b87d0afb4152415fce9f3fae9afac
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-03-09 23:59:21 -0300

    mejora interfaz con nuevos mensajes para el usuario sobre el estado del juego

--- js/main.js
+++ js/main.js
@@ -1,13 +1,73 @@
-const sequenciaMaquina = [];
-const sequenciaUsuario = [];
-const turno = 0;
+const turnoSimon = [];
+const turnoUsuario = [];
 
-function iniciar(){
-  document.querySelector('button') = 12;
+document.querySelector('#iniciar').onclick = function() {
+  const $colores = document.querySelectorAll('.color');
+
+  /* reiniciar(); */
+  bloquearInputUsuario();
+  manejarTurnoSimon($colores);
 }
 
-document.querySelector('button').onclick = function(e) {
-  alert('hi');
-
-  e.preventDefault()
+function manejarTurnoSimon(colores) {
+  actualizarSecuenciaSimon(colores);
+  resaltarColor();
+  manejarTurnoUsuario();
 }
+
+function manejarTurnoUsuario() {
+  desbloquearInputUsuario();
+  actualizarSecuenciaUsuario()
+  const esValido = chequearJugada();
+
+  if(esValido) {
+    manejarTurnoSimon()
+  } else {
+    mostrarMensaje
+  }
+
+}
+
+function bloquearInputUsuario(){
+  document.querySelectorAll('.color').forEach(function(color) {
+    color.onclick = function() {
+      return '';
+    }
+  })
+}
+
+function desbloquearInputUsuario(){
+  document.querySelectorAll('.color').forEach(function(color) {
+    color.onclick = function(event) {
+      console.log(event.target);
+    }
+  })
+}
+
+
+
+
+/* function manejarTurnoSimon(){
+  const $colores = document.querySelectorAll('.color');
+  turnoSimon.push(obtenerNumeroAleatorio($colores))
+
+  turnoSimon.forEach(turno)
+}
+
+function pintarCuadro(cuadro){
+  cuadro.style.opacity = "100%";
+
+  setTimeout(function() {
+  cuadro.style.opacity = "60%";
+  }, 1000);
+}
+
+function obtenerNumeroAleatorio(cuadros) {
+  return Math.floor(Math.random() * cuadros.length)
+}
+
+function obtenerColorAleatorio(colores) {
+  const numeroAleatorio = obtenerNumeroAleatorio(colores)
+  return pintarCuadro(colores[numeroAleatorio]);
+}
+ */
