a3cc59eba418d1900c76d71be0d4f0763cfd49d9
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-03-11 22:37:24 -0300

    refactoriza simondice

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # simon-dice
 // El juego debe comenzar al darle al boton: "Jugar"
-// debe reiniciar Estado a nada (por si se jugó anteriormente)
-// Debe actualizar estado: A TURNO DE SIMÓN.
-// Debe mostrar la primera sencuencia y bloquear el input del usuario.
-// Debe actualizar el estado a: TU TURNO.
-// Debe verificar si la seleccion del jugador es correcta o incorrecta.
+// manejarTurno;
+// simon: -> actualizarSecuencia -> mostrarSecuencia. -> desbloquearUser
+// usuario: ->
--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
-const sequenciaSimon = [];
+let sequenciaSimon = []
 let sequenciaUsuario = [];
+let puntos = 0;
 
-document.querySelector('#iniciar').onclick = function() {
+document.querySelector('#jugar').onclick = comenzarNuevaPartida;
+
+function comenzarNuevaPartida() {
+  document.querySelector('h1').textContent = 'Simón dice';
+  document.querySelector('button').textContent = 'Jugar';
+
+  sequenciaSimon = [];
+  sequenciaUsuario = [];
+  puntos = 0;
+
   manejarTurnoSimon();
 }
 
 function manejarTurnoSimon() {
   const $colores = document.querySelectorAll('.color');
+
+  actualizarTurno('simón');
   bloquearInputUsuario();
-  obtenerColorAleatorio($colores);
-  mostrarSequenciaSimon($colores);
+  actualizarSecuenciaSimon($colores);
+  mostrarSequenciaSimon();
   manejarTurnoUsuario();
 }
 
-function obtenerColorAleatorio(colores) {
-  const colorAleatorio = Math.floor(Math.random() * colores.length);
-  return sequenciaSimon.push(colores[colorAleatorio])
+function manejarTurnoUsuario(){
+  sequenciaUsuario = [];
+
+  setTimeout(function () {
+    actualizarTurno('usuario');
+    desbloquearInputUsuario();
+  }, sequenciaSimon.length * 1000);
+
 }
 
-function mostrarSequenciaSimon() {
-  sequenciaSimon.forEach(function(elemento, indice){
-    const RETRASO_MS = (indice + 1) * 1000
-
-    setTimeout(function () {
-      resaltarColor(elemento)
-    }, RETRASO_MS);
-  })
+function actualizarSecuenciaUsuario(elemento) {
+  sequenciaUsuario.push(elemento);
 }
 
-function resaltarColor(cuadro){
-  cuadro.style.opacity = "100%";
-
-  setTimeout(function() {
-    cuadro.style.opacity = "60%";
-  }, 500);
+function actualizarSecuenciaSimon($colores) {
+  sequenciaSimon.push(obtenerColorAleatorio($colores));
 }
 
 function bloquearInputUsuario(){
@@ -44,40 +51,78 @@ function bloquearInputUsuario(){
   })
 }
 
-function desbloquearInputUsuario(){
-  let contador = 0;
+function mostrarSequenciaSimon(){
+  sequenciaSimon.forEach(function(elemento, indice){
+    const RETRASO_MS = (indice + 1) * 1000
 
-  document.querySelectorAll('.color').forEach(function(color) {
-    color.onclick = function(event) {
-      contador++;
-      const eleccionUsuario = event.target
-      sequenciaUsuario.push(eleccionUsuario)
-      resaltarColor(eleccionUsuario);
-      chequearEleccionUsuario(contador);
-      if(contador === sequenciaSimon.length) {
-        manejarTurnoSimon()
-      }
-    }
+    setTimeout(function () {
+      resaltarColor(elemento);
+    }, RETRASO_MS);
   })
 }
 
-function manejarTurnoUsuario(){
+function reiniciar() {
+  document.querySelector('#color').textContent = 'Hacé click en "Jugar"';
+  document.querySelector('#puntos').textContent = puntos;
+
+  sequenciaSimon = [];
   sequenciaUsuario = [];
-  setTimeout(function() {
-    desbloquearInputUsuario();
-  }, sequenciaSimon.length * 1000);
+  puntos = [];
 }
 
-function chequearEleccionUsuario(contador){
+function obtenerColorAleatorio(colores) {
+  const numeroAleatorio = Math.floor(Math.random() * colores.length);
+  return colores[numeroAleatorio]
+}
 
-  while (contador < sequenciaSimon.length) {
-    sequenciaUsuario.forEach(function(elemento, indice) {
-      if (elemento !== sequenciaSimon[indice]) {
-        alert('perdiste');
-        contador = sequenciaSimon.length;
-      } else {
-        contador ++;
-    }
+function resaltarColor(cuadro){
+  cuadro.style.opacity = "100%";
+
+  setTimeout(function() {
+    cuadro.style.opacity = "60%";
+  }, 500);
+}
+
+function desbloquearInputUsuario() {
+  document.querySelectorAll('.color').forEach(function(color) {
+    color.onclick = manejarInputUsuario;
   })
+}
+
+function manejarInputUsuario(e) {
+  const eleccionUsuario = e.target;
+
+  resaltarColor(eleccionUsuario);
+  actualizarSecuenciaUsuario(eleccionUsuario);
+  validarEleccionUsuario(eleccionUsuario);
+}
+
+function validarEleccionUsuario(color) {
+
+    console.log(sequenciaSimon[sequenciaUsuario.length - 1].id)
+
+    if (color.id !== sequenciaSimon[sequenciaUsuario.length - 1].id) {
+      terminarJuego();
+      return '';
+    }
+
+
+    if (sequenciaSimon.length === sequenciaUsuario.length) {
+      actualizarPuntos();
+      manejarTurnoSimon();
+    }
   }
+
+function actualizarTurno(turno) {
+  document.querySelector('#turno').textContent = turno
+}
+
+function actualizarPuntos() {
+  puntos++;
+  document.querySelector('#puntos').textContent = puntos
+}
+
+function terminarJuego() {
+  document.querySelector('h1').textContent = 'Simón dice: Perdiste';
+  document.querySelector('button').textContent = 'Reiniciar';
 }
